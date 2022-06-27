0db79577b1ef3b24812df159994a30e9f68b1a26
zhensg123 <xyqblog@163.com>
2022-06-27 21:57:02 +0800

    '提酒'

---
+++ src/views/newattr/keypress.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <input v-on:keypress.q="quit">
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+        quit(){
+        }
+    }
+}
+</script>
