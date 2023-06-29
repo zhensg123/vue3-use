8ffc4afb18e06aa5459ab14c169ab3395c80d818
zhenshigang <zhenshigang@xdf.cn>
2023-06-29 17:42:41 +0800

    提交

--- src/views/combined/Ref.vue
+++ src/views/combined/Ref.vue
@@ -2,6 +2,8 @@
     <p>使用vue3的setup，使用ref和reactive</p>
     <div>{{readersNumber}}---{{book}}</div>
     <div ref="refs">测试ref</div>
+
+    {{ user.name }}
 </template>
 <script>
 import {ref, reactive, onMounted} from 'vue'
@@ -10,16 +12,20 @@ export default {
        const readersNumber = ref(0)
            const refs = ref(null)
       const book = reactive({ title: 'Vue 3 Guide' })
-      
+      const user = ref({ name: 'Alice' })
+
       onMounted(() => {
         // DOM 元素将在初始渲染后分配给 ref
         console.log(refs.value, '33333')
+        console.log(user.value, '33111333')
+
       })
       // 暴露给 template
       return {
         readersNumber,
         book,
-        refs
+        refs,
+        user
       }
     }
 }
